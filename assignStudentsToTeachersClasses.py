32cbbd326bad895ffec3e78dbd8e25071ab1b3a2
Tudor3510 <tudorcioc@gmail.com>
2024-10-23 15:02:43 +0300

    Moved to linear programming

--- assignStudentsToTeachersClasses.py
+++ assignStudentsToTeachersClasses.py
@@ -1,5 +1,5 @@
+from pulp import LpMaximize, LpProblem, LpVariable, lpSum
 from itertools import combinations
-
 
 def suprapunere_intervale(interval1, interval2):
     def parse_interval(interval):
@@ -24,7 +24,7 @@ reader = open("input.txt", "r")
 
 line = reader.readline()
 profesor_disponibilitate = {}
-while line != "\n" and line != "": 
+while line != "\n" and line != "":
     input = line.strip().split(":", 1)
     if "-" not in input[1].strip():
         line = reader.readline()
@@ -32,7 +32,6 @@ while line != "\n" and line != "":
     profesor_disponibilitate[input[0].strip()] = [tuple(map(str.strip, hours.strip().split("-"))) for hours in input[1].split(",")]
     
     line = reader.readline()
-
 
 elevi = []
 while line != "":
@@ -47,39 +46,16 @@ while line != "":
     toAppend["disponibilitate"] = {}
     
     line = reader.readline()
-    while line != "\n" and line != "": 
+    while line != "\n" and line != "":
         input = line.strip().split(":", 1)
         if "-" not in input[1].strip():
             line = reader.readline()
             continue
         toAppend["disponibilitate"][input[0].strip()] = [tuple(map(str.strip, hours.strip().split("-"))) for hours in input[1].split(",")]
-    
+        
         line = reader.readline()
         
     elevi.append(toAppend)
-
-
-# elevi = [
-#     {"nume": "Alex Radulescu", "dificultate": 8, "disponibilitate": {
-#                                         "Mo": [("10:00", "14:00"), ("18:00", "20:00")],
-#                                         "We": [("13:00", "15:00")],
-#                                         "Fr": [("14:00", "16:00")],
-#                                         "Sa": [("16:00", "18:00")],
-#                                         "Su": [("10:00", "12:00")]}},
-#     {"nume": "Mihai Ionescu", "dificultate": 7, "disponibilitate": {
-#                                         "Mo": [("12:00", "16:00")],
-#                                         "Tu": [("11:00", "13:00")],
-#                                         "Fr": [("14:00", "16:00")],
-#                                         "Su": [("10:00", "12:00")]}},
-#     {"nume": "Radu Constantin", "dificultate": 9, "disponibilitate": {
-#                                         "We": [("10:00", "12:00")]}},
-#     {"nume": "Iacob Deleanu", "dificultate": 6, "disponibilitate": {
-#                                         "Mo": [("10:00", "12:00")],
-#                                         "Tu": [("14:00", "16:00")],
-#                                         "We": [("10:00", "12:00"), ("18:00", "20:00")],
-#                                         "Fr": [("15:00", "17:00")]}}
-# ]
-
 
 # profesor_disponibilitate = {
 #     "Mo": [("08:00", "10:00"), ("14:00", "20:00")],
@@ -91,60 +67,48 @@ while line != "":
 #     "Su": [("10:00", "12:00"), ("13:00", "15:00")]
 # }
 
-
 n = 4
 m = 2
 
+model = LpProblem(name="maximal-student-allocation", sense=LpMaximize)
 
-def verifica_grupa(grupa, m):
-    dificultati = [e['dificultate'] for e in grupa]
-    return max(dificultati) - min(dificultati) <= m
-
-
-def backtracking_solutie(elevi, n, m, profesor_disponibilitate):
-    max_elevi_alocati = 0
-    solutie_optimala = []
-    elevi_nealocati = elevi.copy()
-
-
-    def backtrack(elevi_ramas, grupe_curente, elevi_alocati):
-        nonlocal max_elevi_alocati, solutie_optimala, elevi_nealocati
-
-
-        if elevi_alocati > max_elevi_alocati:
-            max_elevi_alocati = elevi_alocati
-            solutie_optimala = grupe_curente[:]
-            elevi_nealocati = elevi_ramas[:]
+x = {}
+for zi, intervale in profesor_disponibilitate.items():
+    for interval_prof in intervale:
+        for grupa_dim in range(1, n + 1):
+            for grupa in combinations(elevi, grupa_dim):
+                if all(any(suprapunere_intervale(interval_prof, elev_interval)
+                        for elev_interval in elev["disponibilitate"].get(zi, [])) for elev in grupa):
+                    if max(e["dificultate"] for e in grupa) - min(e["dificultate"] for e in grupa) <= m:
+                        grupa_key = tuple(e["nume"] for e in grupa)
+                        var_name = f"x_{zi}_{interval_prof}_{'_'.join(grupa_key)}"
+                        x[(zi, interval_prof, grupa_key)] = LpVariable(var_name, cat='Binary')
 
 
-        for zi, intervale_prof in profesor_disponibilitate.items():
-            for interval_prof in intervale_prof:
-
-                for grupa_dim in range(1, n + 1):
-                    for grupa in combinations(elevi_ramas, grupa_dim):
-
-                        if verifica_grupa(grupa, m):
-                            if all(any(suprapunere_intervale(interval_prof, elev_interval)
-                            for elev_interval in elev['disponibilitate'].get(zi, [])) for elev in grupa):
+model += lpSum(len(grupa_key) * x[(zi, interval_prof, grupa_key)] for (zi, interval_prof, grupa_key) in x), "Total_elevi_alocati"
 
 
-                                elevi_ramas_nou = [elev for elev in elevi_ramas if elev not in grupa]
-                                backtrack(elevi_ramas_nou, grupe_curente + [(zi, interval_prof, grupa)], elevi_alocati + len(grupa))
+for elev in elevi:
+    model += lpSum(x[(zi, interval_prof, grupa_key)]
+                   for (zi, interval_prof, grupa_key) in x if elev["nume"] in grupa_key) <= 1, f"Restrictie_{elev['nume']}"
 
 
-    backtrack(elevi, [], 0)
-
-    return solutie_optimala, elevi_nealocati
+model.solve()
 
 
+grupe_formate = []
+for (zi, interval_prof, grupa_key), var in x.items():
+    if var.varValue == 1:
+        grupe_formate.append((zi, interval_prof, grupa_key))
 
-solutie, elevi_nealocati = backtracking_solutie(elevi, n, m, profesor_disponibilitate)
+print("Grupe formate (solutie optima cu programare liniara):")
+for zi, interval, grupa_key in grupe_formate:
+    print(f"Ziua {zi}, intervalul {interval}: {list(grupa_key)}")
 
 
-print("Grupe formate (solutie optima cu backtracking):")
-for zi, interval, grupa in solutie:
-    print(f"Ziua {zi}, intervalul {interval}: {[elev['nume'] for elev in grupa]}")
+elevi_alocati = {nume for (_, _, grupa_key) in grupe_formate for nume in grupa_key}
+elevi_nealocati = [elev for elev in elevi if elev["nume"] not in elevi_alocati]
 
 print("\nElevi nealocati:")
 for elev in elevi_nealocati:
-    print(elev['nume'])
+    print(elev["nume"])
