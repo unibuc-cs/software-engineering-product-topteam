68c2b50ee78b7f22f961ab4d46e054a698c9bbaa
Teodor Maciuca <108132918+teodorma@users.noreply.github.com>
2024-11-22 19:56:43 +0200

    Remade controllers and repositories

--- backend-MT/Controllers/UserController.cs
+++ backend-MT/Controllers/UserController.cs
@@ -1,73 +1,131 @@
-﻿//using backend_MT.Models;
-//using backend_MT.Service.ElevService;
-//using Microsoft.AspNetCore.Mvc;
-
-//namespace backend_MT.Controllers
-//{
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    public class UserController : ControllerBase
-//    {
-//        private readonly IElevService _elevService;
-
-//        public UserController(IElevService elevService)
-//        {
-//            _elevService = elevService;
-//        }
-
-//        // GET: api/elev
-//        [HttpGet]
-//        public async Task<ActionResult<IEnumerable<User>>> GetAllStudents()
-//        {
-//            var elevi = await _elevService.GetAllStudentsAsync();
-//            return Ok(elevi);
-//        }
-
-//        // GET: api/elev/{id}
-//        [HttpGet("{id}")]
-//        public async Task<ActionResult<User>> GetStudentById(string id)
-//        {
-//            var elev = await _elevService.GetStudentByIdAsync(id);
-//            if (elev == null)
-//            {
-//                return NotFound();
-//            }
-//            return Ok(elev);
-//        }
-
-//        // POST: api/elev
-//        [HttpPost]
-//        public async Task<ActionResult<User>> AddStudent(User elev)
-//        {
-//            await _elevService.AddStudentAsync(elev);
-//            return CreatedAtAction(nameof(GetStudentById), new { id = elev.Id }, elev); // Asumând că Elev are o proprietate Id
-//        }
-
-//        // PUT: api/elev/{id}
-//        [HttpPut("{id}")]
-//        public async Task<IActionResult> UpdateStudent(string id, User elev)
-//        {
-//            if (id != elev.Id) // Verifică dacă ID-urile se potrivesc
-//            {
-//                return BadRequest();
-//            }
-
-//            await _elevService.UpdateStudentAsync(elev);
-//            return NoContent();
-//        }
-
-//        // DELETE: api/elev/{id}
-//        [HttpDelete("{id}")]
-//        public async Task<IActionResult> DeleteStudent(string id)
-//        {
-//            var elev = await _elevService.GetStudentByIdAsync(id);
-//            if (elev == null)
-//            {
-//                return NotFound();
-//            }
-
-//            await _elevService.DeleteStudentAsync(id);
-//            return NoContent();
-//        }
-//    }
-//}
+﻿// using backend_MT.Models;
+// using backend_MT.Service.ElevService;
+// using backend_MT.Service.ProfesorService;
+// using Microsoft.AspNetCore.Mvc;
+//
+// namespace backend_MT.Controllers
+// {
+//     [Route("api/[controller]")]
+//     [ApiController]
+//     public class UserController : ControllerBase
+//     {
+//         private readonly IElevService _elevService;
+//         private readonly IProfesorService _profesorService;
+//
+//         public UserController(IElevService elevService, IProfesorService profesorService)
+//         {
+//             _elevService = elevService;
+//             _profesorService = profesorService;
+//         }
+//
+//         // GET: api/user/students
+//         [HttpGet("students")]
+//         public async Task<ActionResult<IEnumerable<User>>> GetAllStudents()
+//         {
+//             var students = await _elevService.GetAllStudentsAsync();
+//             return Ok(students);
+//         }
+//
+//         // GET: api/user/teachers
+//         [HttpGet("teachers")]
+//         public async Task<ActionResult<IEnumerable<User>>> GetAllTeachers()
+//         {
+//             var teachers = await _profesorService.GetAllTeachersAsync();
+//             return Ok(teachers);
+//         }
+//
+//         // GET: api/user/students/{id}
+//         [HttpGet("students/{id}")]
+//         public async Task<ActionResult<User>> GetStudentById(string id)
+//         {
+//             var student = await _elevService.GetStudentByIdAsync(id);
+//             if (student == null)
+//             {
+//                 return NotFound();
+//             }
+//             return Ok(student);
+//         }
+//
+//         // GET: api/user/teachers/{id}
+//         [HttpGet("teachers/{id}")]
+//         public async Task<ActionResult<User>> GetTeacherById(int id)
+//         {
+//             var teacher = await _profesorService.GetTeacherByIdAsync(id);
+//             if (teacher == null)
+//             {
+//                 return NotFound();
+//             }
+//             return Ok(teacher);
+//         }
+//
+//         // POST: api/user/students
+//         [HttpPost("students")]
+//         public async Task<ActionResult<User>> AddStudent(User student)
+//         {
+//             await _elevService.AddStudentAsync(student);
+//             return CreatedAtAction(nameof(GetStudentById), new { id = student.Id }, student);
+//         }
+//
+//         // POST: api/user/teachers
+//         [HttpPost("teachers")]
+//         public async Task<ActionResult<User>> AddTeacher(User teacher)
+//         {
+//             await _profesorService.AddTeacherAsync(teacher);
+//             return CreatedAtAction(nameof(GetTeacherById), new { id = teacher.Id }, teacher);
+//         }
+//
+//         // PUT: api/user/students/{id}
+//         [HttpPut("students/{id}")]
+//         public async Task<IActionResult> UpdateStudent(int id, User student)
+//         {
+//             if (id != student.Id)
+//             {
+//                 return BadRequest();
+//             }
+//
+//             await _elevService.UpdateStudentAsync(student);
+//             return NoContent();
+//         }
+//
+//         // PUT: api/user/teachers/{id}
+//         [HttpPut("teachers/{id}")]
+//         public async Task<IActionResult> UpdateTeacher(int id, User teacher)
+//         {
+//             if (id != teacher.Id)
+//             {
+//                 return BadRequest();
+//             }
+//
+//             await _profesorService.UpdateTeacherAsync(teacher);
+//             return NoContent();
+//         }
+//
+//         // DELETE: api/user/students/{id}
+//         [HttpDelete("students/{id}")]
+//         public async Task<IActionResult> DeleteStudent(string id)
+//         {
+//             var student = await _elevService.GetStudentByIdAsync(id);
+//             if (student == null)
+//             {
+//                 return NotFound();
+//             }
+//
+//             await _elevService.DeleteStudentAsync(id);
+//             return NoContent();
+//         }
+//
+//         // DELETE: api/user/teachers/{id}
+//         [HttpDelete("teachers/{id}")]
+//         public async Task<IActionResult> DeleteTeacher(int id)
+//         {
+//             var teacher = await _profesorService.GetTeacherByIdAsync(id);
+//             if (teacher == null)
+//             {
+//                 return NotFound();
+//             }
+//
+//             await _profesorService.DeleteTeacherAsync(id);
+//             return NoContent();
+//         }
+//     }
+// }
--- backend-MT/Repositories/CursRepository/CursRepository.cs
+++ backend-MT/Repositories/CursRepository/CursRepository.cs
@@ -1,50 +1,50 @@
-//using backend_MT.Data;
-//using backend_MT.Models;
-//using Microsoft.EntityFrameworkCore;
-//using System.Collections.Generic;
-//using System.Threading.Tasks;
+using backend_MT.Data;
+using backend_MT.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-//namespace backend_MT.Repositories.CursRepository
-//{
-//    public class CursRepository : ICursRepository
-//    {
-//        private readonly ApplicationDbContext _context;
+namespace backend_MT.Repositories.CursRepository
+{
+    public class CursRepository : ICursRepository
+    {
+        private readonly ApplicationDbContext _context;
 
-//        public CursRepository(ApplicationDbContext context)
-//        {
-//            _context = context;
-//        }
+        public CursRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
 
-//        public async Task<IEnumerable<Curs>> GetAllCoursesAsync()
-//        {
-//            return await _context.Cursuri.ToListAsync();
-//        }
+        public async Task<IEnumerable<Curs>> GetAllCoursesAsync()
+        {
+            return await _context.curs.ToListAsync();
+        }
 
-//        public async Task<Curs> GetCourseByIdAsync(int id)
-//        {
-//            return await _context.Cursuri.FindAsync(id);
-//        }
+        public async Task<Curs> GetCourseByIdAsync(int id)
+        {
+            return await _context.curs.FindAsync(id);
+        }
 
-//        public async Task AddCourseAsync(Curs curs)
-//        {
-//            await _context.Cursuri.AddAsync(curs);
-//            await _context.SaveChangesAsync();
-//        }
+        public async Task AddCourseAsync(Curs curs)
+        {
+            await _context.curs.AddAsync(curs);
+            await _context.SaveChangesAsync();
+        }
 
-//        public async Task UpdateCourseAsync(Curs curs)
-//        {
-//            _context.Cursuri.Update(curs);
-//            await _context.SaveChangesAsync();
-//        }
+        public async Task UpdateCourseAsync(Curs curs)
+        {
+            _context.curs.Update(curs);
+            await _context.SaveChangesAsync();
+        }
 
-//        public async Task DeleteCourseAsync(int id)
-//        {
-//            var curs = await _context.Cursuri.FindAsync(id);
-//            if (curs != null)
-//            {
-//                _context.Cursuri.Remove(curs);
-//                await _context.SaveChangesAsync();
-//            }
-//        }
-//    }
-//}
+        public async Task DeleteCourseAsync(int id)
+        {
+            var curs = await _context.curs.FindAsync(id);
+            if (curs != null)
+            {
+                _context.curs.Remove(curs);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
