678b9cd47d0fe0fa2c3d416447d9afb0f5a0fbd8
Teodor Maciuca <108132918+teodorma@users.noreply.github.com>
2024-12-11 14:40:11 +0200

    Update Program.cs

--- backend-MT/Program.cs
+++ backend-MT/Program.cs
@@ -1,24 +1,34 @@
 using backend_MT.Data;
 using backend_MT.Models;
+using backend_MT.Repositories.AbonamentRepository;
 using backend_MT.Repositories.CursRepository;
+using backend_MT.Repositories.DisponibilitateRepository;
 //using backend_MT.Repositories.ElevRepository;
 using backend_MT.Repositories.FeedbackRepository;
 using backend_MT.Repositories.GrupaRepository;
 using backend_MT.Repositories.MaterialeRepository;
 using backend_MT.Repositories.MesajRepository;
 using backend_MT.Repositories.NotificareRepository;
+using backend_MT.Repositories.ParticipareGrupaRepository;
 using backend_MT.Repositories.PlataRepository;
+using backend_MT.Repositories.PredareRepository;
+using backend_MT.Repositories.PrezentaRepository;
 using backend_MT.Repositories.RaspunsTemaRepository;
 using backend_MT.Repositories.SedintaRepository;
 using backend_MT.Repositories.SupportRepository;
 using backend_MT.Repositories.TemaRepository;
+using backend_MT.Service.AbonamentService;
 using backend_MT.Service.CursService;
+using backend_MT.Service.DisponibilitateService;
 using backend_MT.Service.FeedbackService;
 using backend_MT.Service.GrupaService;
 using backend_MT.Service.MaterialeService;
 using backend_MT.Service.MesajService;
 using backend_MT.Service.NotificareService;
+using backend_MT.Service.ParticipareGrupaService;
 using backend_MT.Service.PlataService;
+using backend_MT.Service.PredareService;
+using backend_MT.Service.PrezentaService;
 using backend_MT.Service.RaspunsTemaService;
 using backend_MT.Service.SedintaService;
 using backend_MT.Service.SupportService;
@@ -31,40 +41,53 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 // Configure Entity Framework Core with SQL Server
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+if (!builder.Environment.IsEnvironment("Test"))
+{
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+}
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Register services and repositories
-builder.Services.AddScoped<ICursService, CursService>();
+// Repositories
+builder.Services.AddScoped<IAbonamentRepository, AbonamentRepository>();
 builder.Services.AddScoped<ICursRepository, CursRepository>();
-//builder.Services.AddScoped<IElevService, ElevService>();
-//builder.Services.AddScoped<IElevRepository, ElevRepository>();
-//builder.Services.AddScoped<IFeedbackService, FeedbackService>();
-//builder.Services.AddScoped<IFeedbackRepository, FeedbackRepository>();
-//builder.Services.AddScoped<IGrupaService, GrupaService>();
-//builder.Services.AddScoped<IGrupaRepository, GrupaRepository>();
-//builder.Services.AddScoped<IMaterialService, MaterialService>();
-//builder.Services.AddScoped<IMaterialRepository, MaterialRepository>();
-//builder.Services.AddScoped<IMesajService, MesajService>();
-//builder.Services.AddScoped<IMesajRepository, MesajRepository>();
-//builder.Services.AddScoped<INotificareService, NotificareService>();
-//builder.Services.AddScoped<INotificareRepository, NotificareRepository>();
-//builder.Services.AddScoped<IPlataRepository, PlataRepository>();
-//builder.Services.AddScoped<IProfesorRepository, ProfesorRepository>();
-//builder.Services.AddScoped<IPlataService, PlataService>();
-//builder.Services.AddScoped<IProfesorService, ProfesorService>();
-//builder.Services.AddScoped<IRaspunsTemaRepository, RaspunsTemaRepository>();
-//builder.Services.AddScoped<ISedintaRepository, SedintaRepository>();
-//builder.Services.AddScoped<IRaspunsTemaService, RaspunsTemaService>();
-//builder.Services.AddScoped<ISedintaService, SedintaService>();
-//builder.Services.AddScoped<ISupportRepository, SupportRepository>();
-//builder.Services.AddScoped<ITemaRepository, TemaRepository>();
-//builder.Services.AddScoped<ISupportService, SupportService>();
-//builder.Services.AddScoped<ITemaService, TemaService>();
+builder.Services.AddScoped<IDisponibilitateRepository, DisponibilitateRepository>();
+builder.Services.AddScoped<IFeedbackRepository, FeedbackRepository>();
+builder.Services.AddScoped<IGrupaRepository, GrupaRepository>();
+builder.Services.AddScoped<IMaterialRepository, MaterialRepository>();
+builder.Services.AddScoped<IMesajRepository, MesajRepository>();
+builder.Services.AddScoped<INotificareRepository, NotificareRepository>();
+builder.Services.AddScoped<IPlataRepository, PlataRepository>();
+builder.Services.AddScoped<IPredareRepository, PredareRepository>();
+builder.Services.AddScoped<IPrezentaRepository, PrezentaRepository>();
+builder.Services.AddScoped<IRaspunsTemaRepository, RaspunsTemaRepository>();
+builder.Services.AddScoped<ISedintaRepository, SedintaRepository>();
+builder.Services.AddScoped<ISupportRepository, SupportRepository>();
+builder.Services.AddScoped<ITemaRepository, TemaRepository>();
+builder.Services.AddScoped<IParticipareGrupaRepository, ParticipareGrupaRepository>();
+
+// Services
+builder.Services.AddScoped<IAbonamentService, AbonamentService>();
+builder.Services.AddScoped<ICursService, CursService>();
+builder.Services.AddScoped<IDisponibilitateService, DisponibilitateService>();
+builder.Services.AddScoped<IFeedbackService, FeedbackService>();
+builder.Services.AddScoped<IGrupaService, GrupaService>();
+builder.Services.AddScoped<IMaterialService, MaterialService>();
+builder.Services.AddScoped<IMesajService, MesajService>();
+builder.Services.AddScoped<INotificareService, NotificareService>();
+builder.Services.AddScoped<IPlataService, PlataService>();
+builder.Services.AddScoped<IPredareService, PredareService>();
+builder.Services.AddScoped<IPrezentaService, PrezentaService>();
+builder.Services.AddScoped<IRaspunsTemaService, RaspunsTemaService>();
+builder.Services.AddScoped<ISedintaService, SedintaService>();
+builder.Services.AddScoped<ISupportService, SupportService>();
+builder.Services.AddScoped<ITemaService, TemaService>();
+builder.Services.AddScoped<IParticipareGrupaService, ParticipareGrupaService>();
+
+
 
 var app = builder.Build();
 
@@ -80,3 +103,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+public partial class Program { }
