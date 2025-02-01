3a6c40d72fb7adb431f866a76ce9c8191c6d9812
AlexCodreanu23 <AlexCosmin2377@gmail.com>
2025-02-01 19:17:34 +0200

    added k6 stress test

---
+++ backend-MT.Tests/PerformanceTests/stressTest.js
@@ -0,0 +1,26 @@
+﻿import http from 'k6/http';
+import { sleep } from 'k6';
+import { check } from 'k6';
+
+export let options = {
+    scenarios: {
+        stress_test: {
+            executor: 'ramping-vus',
+            startVUs: 0,            
+            stages: [
+                { duration: '10m', target: 1000 }, 
+            ],
+            gracefulStop: '30s',    
+        },
+    },
+};
+
+export default function () {
+    let res = http.get('http://localhost:5081/api/curs');
+
+    check(res, {
+        'status is 200': (r) => r.status === 200,
+    });
+
+    sleep(1);
+}
